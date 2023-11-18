4e3fe8206b5e85d12f96b5c5dedd2135a3168b4f
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-18 10:28:27 -0700

    kmeans first draft

--- HW3/kmeans_analysis.py
+++ HW3/kmeans_analysis.py
@@ -8,11 +8,35 @@ def read_csv_get_X_y():
 def get_num_clusters(y):
     return len(np.unique(y))
 
-def kmeans(X, y, num_clusters):
+def euclidean_distance(first, second):
+    diff = first - second
+    squared = diff * diff
+    sum_of_squares = squared.sum()
+    distance = np.sqrt(sum_of_squares)
+    return distance
+
+def kmeans_max_iterations(X, y, num_clusters, distance_fn, max_iterations):
     X_len = len(X)
+    random_idx_list = np.random.choice(range(X_len), size=num_clusters, replace=False)
+    centroids = X[random_idx_list]
+
+    for i in range(max_iterations):
+        print(f"Iteration {i}", end='\r')
+        pts_in_centroids = [[] for _ in range(num_clusters)]
+
+        for pt_idx in range(X_len):
+            distances = [distance_fn(centroid, X[pt_idx]) for centroid in centroids]
+            idx_closest_centroid = np.argmin(distances)
+            pts_in_centroids[idx_closest_centroid].append(pt_idx)
+
+        new_centroids = [X[cent_group].mean(axis=0) for cent_group in pts_in_centroids]
+        
+    return new_centroids
+
 
 
 if __name__ == "__main__":
     X, y = read_csv_get_X_y()
     num_clusters = get_num_clusters(y)
-    kmeans(X, y, num_clusters)
+    centroids = kmeans_max_iterations(X, y, num_clusters=num_clusters, distance_fn=euclidean_distance, max_iterations=50)
+    print(centroids)
