901ee79ca1cb2f3d15ac883aa0f279f7e81c697d
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-18 14:48:37 -0700

    SSE increase

--- HW3/kmeans_analysis.py
+++ HW3/kmeans_analysis.py
@@ -94,6 +94,40 @@ def kmeans_until_convergence(X, y, num_clusters, distance_fn):
         
     return new_centroids, pts_in_centroids, sse_value
 
+def kmeans_until_sse_inc(X, y, num_clusters, distance_fn):
+    np.random.seed(69)
+    X_len = len(X)
+    random_idx_list = np.random.choice(range(X_len), size=num_clusters, replace=False)
+    centroids = X[random_idx_list]
+    iter = 0
+    cur_sse = 694206942069420
+
+    while True:
+        print(f"Iteration {iter}", end='\r')
+        iter += 1
+        pts_in_centroids = [[] for _ in range(num_clusters)]
+
+        for pt_idx in range(X_len):
+            distances = [distance_fn(centroid, X[pt_idx]) for centroid in centroids]
+            idx_closest_centroid = np.argmin(distances)
+            pts_in_centroids[idx_closest_centroid].append(pt_idx)
+
+        new_centroids = [X[cent_group].mean(axis=0) for cent_group in pts_in_centroids]
+        new_sse = sse(X, centroids, pts_in_centroids)
+
+        # additional condition will mark both sse similar if they're same to the 4th decimal position
+        # to avoid infinite looping
+        if new_sse > cur_sse or round(new_sse, 4) == round(cur_sse, 4):
+            print("\nConverged at iteration", iter)
+            break
+
+        centroids = new_centroids
+        cur_sse = new_sse
+
+    sse_value = sse(X, centroids, pts_in_centroids)
+        
+    return new_centroids, pts_in_centroids, sse_value
+
 def get_labels_clustered_data(X, y, pts_in_centroids):
     labeled_data = []
     
@@ -146,21 +180,38 @@ if __name__ == "__main__":
 
     # Q3
 
+    # print('Euclidean Distance:')
+    # euc_conv_centroids, euc_conv_pts_in_centroids, euc_conv_sse = kmeans_until_convergence(X, y, num_clusters=num_clusters, distance_fn=euclidean_distance)
+    # print()
+
+    # print('Cosine Distance:')
+    # cos_conv_centroids, cos_conv_pts_in_centroids, cos_conv_sse = kmeans_until_convergence(X, y, num_clusters=num_clusters, distance_fn=cosine_distance)
+    # print()
+
+    # print('Generalized Jaccard Distance:')
+    # jac_conv_centroids, jac_conv_pts_in_centroids, jac_conv_sse = kmeans_until_convergence(X, y, num_clusters=num_clusters, distance_fn=generalized_jaccard_distance)
+    # print()
+
     print('Euclidean Distance:')
-    euc_conv_centroids, euc_conv_pts_in_centroids, euc_conv_sse = kmeans_until_convergence(X, y, num_clusters=num_clusters, distance_fn=euclidean_distance)
+    euc_sse_centroids, euc_sse_pts_in_centroids, euc_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=euclidean_distance)
     print()
 
     print('Cosine Distance:')
-    cos_conv_centroids, cos_conv_pts_in_centroids, cos_conv_sse = kmeans_until_convergence(X, y, num_clusters=num_clusters, distance_fn=cosine_distance)
+    cos_sse_centroids, cos_sse_pts_in_centroids, cos_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=cosine_distance)
     print()
 
     print('Generalized Jaccard Distance:')
-    jac_conv_centroids, jac_conv_pts_in_centroids, jac_conv_sse = kmeans_until_convergence(X, y, num_clusters=num_clusters, distance_fn=generalized_jaccard_distance)
+    jac_sse_centroids, jac_sse_pts_in_centroids, jac_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=generalized_jaccard_distance)
     print()
 
     # Q4
 
-    print(f'SSE - Euclidean Distance   - Until No change in centroid position: {euc_conv_sse}')
-    print(f'SSE - Cosine Distance      - Until No change in centroid position: {cos_conv_sse}')
-    print(f'SSE - Gen Jaccard Distance - Until No change in centroid position: {jac_conv_sse}')
+    # print(f'SSE - Euclidean Distance   - Until No change in centroid position: {euc_conv_sse}')
+    # print(f'SSE - Cosine Distance      - Until No change in centroid position: {cos_conv_sse}')
+    # print(f'SSE - Gen Jaccard Distance - Until No change in centroid position: {jac_conv_sse}')
+    # print()
+
+    print(f'SSE - Euclidean Distance   - Until SSE increases: {euc_sse_sse}')
+    print(f'SSE - Cosine Distance      - Until SSE increases: {cos_sse_sse}')
+    print(f'SSE - Gen Jaccard Distance - Until SSE increases: {jac_sse_sse}')
     print()
