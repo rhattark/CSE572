ddb940962f2329f738dfd03401674f9110877f62
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-18 09:44:26 -0700

    kmeans num clusters

--- HW3/kmeans_analysis.py
+++ HW3/kmeans_analysis.py
@@ -1,13 +1,18 @@
-import pandas as pd
+import numpy as np
 
 def read_csv_get_X_y():
-    X = pd.read_csv('kmeans_data/data.csv')
-    y = pd.read_csv('kmeans_data/label.csv')
+    X = np.genfromtxt('kmeans_data/data.csv', delimiter=',')
+    y = np.genfromtxt('kmeans_data/label.csv', delimiter=',')
     return X, y
 
-def kmeans(X, y):
-    pass
+def get_num_clusters(y):
+    return len(np.unique(y))
+
+def kmeans(X, y, num_clusters):
+    X_len = len(X)
+
 
 if __name__ == "__main__":
     X, y = read_csv_get_X_y()
-    kmeans(X, y)
+    num_clusters = get_num_clusters(y)
+    kmeans(X, y, num_clusters)
