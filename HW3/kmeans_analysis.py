7201cd11189315d55101f273b211c0be429311d6
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-18 14:58:00 -0700

    until max iterations

--- HW3/kmeans_analysis.py
+++ HW3/kmeans_analysis.py
@@ -128,6 +128,28 @@ def kmeans_until_sse_inc(X, y, num_clusters, distance_fn):
         
     return new_centroids, pts_in_centroids, sse_value
 
+def kmeans_until_max_iter(X, y, num_clusters, distance_fn, max_iterations):
+    np.random.seed(69)
+    X_len = len(X)
+    random_idx_list = np.random.choice(range(X_len), size=num_clusters, replace=False)
+    centroids = X[random_idx_list]
+
+    for iter in range(max_iterations):
+        print(f"Iteration {iter}", end='\r')
+        pts_in_centroids = [[] for _ in range(num_clusters)]
+
+        for pt_idx in range(X_len):
+            distances = [distance_fn(centroid, X[pt_idx]) for centroid in centroids]
+            idx_closest_centroid = np.argmin(distances)
+            pts_in_centroids[idx_closest_centroid].append(pt_idx)
+
+        new_centroids = [X[cent_group].mean(axis=0) for cent_group in pts_in_centroids]
+        centroids = new_centroids
+
+    sse_value = sse(X, centroids, pts_in_centroids)
+        
+    return new_centroids, pts_in_centroids, sse_value
+
 def get_labels_clustered_data(X, y, pts_in_centroids):
     labeled_data = []
     
@@ -192,16 +214,28 @@ if __name__ == "__main__":
     # jac_conv_centroids, jac_conv_pts_in_centroids, jac_conv_sse = kmeans_until_convergence(X, y, num_clusters=num_clusters, distance_fn=generalized_jaccard_distance)
     # print()
 
+    # print('Euclidean Distance:')
+    # euc_sse_centroids, euc_sse_pts_in_centroids, euc_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=euclidean_distance)
+    # print()
+
+    # print('Cosine Distance:')
+    # cos_sse_centroids, cos_sse_pts_in_centroids, cos_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=cosine_distance)
+    # print()
+
+    # print('Generalized Jaccard Distance:')
+    # jac_sse_centroids, jac_sse_pts_in_centroids, jac_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=generalized_jaccard_distance)
+    # print()
+
     print('Euclidean Distance:')
-    euc_sse_centroids, euc_sse_pts_in_centroids, euc_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=euclidean_distance)
+    euc_ite_centroids, euc_ite_pts_in_centroids, euc_ite_sse = kmeans_until_max_iter(X, y, num_clusters=num_clusters, distance_fn=euclidean_distance, max_iterations=100)
     print()
 
     print('Cosine Distance:')
-    cos_sse_centroids, cos_sse_pts_in_centroids, cos_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=cosine_distance)
+    cos_ite_centroids, cos_ite_pts_in_centroids, cos_ite_sse = kmeans_until_max_iter(X, y, num_clusters=num_clusters, distance_fn=cosine_distance, max_iterations=100)
     print()
 
     print('Generalized Jaccard Distance:')
-    jac_sse_centroids, jac_sse_pts_in_centroids, jac_sse_sse = kmeans_until_sse_inc(X, y, num_clusters=num_clusters, distance_fn=generalized_jaccard_distance)
+    jac_ite_centroids, jac_ite_pts_in_centroids, jac_ite_sse = kmeans_until_max_iter(X, y, num_clusters=num_clusters, distance_fn=generalized_jaccard_distance, max_iterations=100)
     print()
 
     # Q4
@@ -211,7 +245,12 @@ if __name__ == "__main__":
     # print(f'SSE - Gen Jaccard Distance - Until No change in centroid position: {jac_conv_sse}')
     # print()
 
-    print(f'SSE - Euclidean Distance   - Until SSE increases: {euc_sse_sse}')
-    print(f'SSE - Cosine Distance      - Until SSE increases: {cos_sse_sse}')
-    print(f'SSE - Gen Jaccard Distance - Until SSE increases: {jac_sse_sse}')
+    # print(f'SSE - Euclidean Distance   - Until SSE increases: {euc_sse_sse}')
+    # print(f'SSE - Cosine Distance      - Until SSE increases: {cos_sse_sse}')
+    # print(f'SSE - Gen Jaccard Distance - Until SSE increases: {jac_sse_sse}')
+    # print()
+
+    print(f'SSE - Euclidean Distance   - Until max iterations: {euc_ite_sse}')
+    print(f'SSE - Cosine Distance      - Until max iterations: {cos_ite_sse}')
+    print(f'SSE - Gen Jaccard Distance - Until max iterations: {jac_ite_sse}')
     print()
